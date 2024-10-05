e73dcd3f26edf967d8c0ee1b0c5e853f3ccd13ec
Rosario Borgesi <borgesiros@gmail.com>
2024-10-05 13:08:06 +0200

    Removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -8,50 +8,23 @@ import {
 import { Button, Image, Link, Input, Divider } from "@nextui-org/react";
 import { useLogin, usePrivy } from "@privy-io/react-auth";
 import { useSmartWallets } from "@privy-io/react-auth/smart-wallets";
-import {
-  CopyIcon,
-  RefreshCcw,
-  CheckIcon,
-  Pen,
-  Send,
-  ChevronDown,
-  ChevronUp,
-  LogOut,
-  BookOpen,
-  Github,
-} from "lucide-react";
+import { CopyIcon, CheckIcon, Send, LogOut } from "lucide-react";
 import { useState, useCallback, useEffect } from "react";
-import {
-  encodeFunctionData,
-  erc20Abi,
-  erc721Abi,
-  formatUnits,
-  parseEther,
-  parseUnits,
-} from "viem";
+import { encodeFunctionData, erc20Abi, parseUnits } from "viem";
 import { base, baseSepolia } from "viem/chains";
 import { useChainId, useSwitchChain, useBalance, useReadContract } from "wagmi";
-import { formatEther } from "viem";
 import { proofifiAbi } from "@/lib/proofifiAbi";
 
 export default function Home() {
   const { ready, authenticated, logout, user } = usePrivy();
   const [message, setMessage] = useState("");
   const [copiedWallet, setCopiedWallet] = useState<string | null>(null);
-  const [signedMessage, setSignedMessage] = useState("");
-  const [isSignedMessageExpanded, setIsSignedMessageExpanded] = useState(false);
-  const [copiedSignedMessage, setCopiedSignedMessage] = useState(false);
   const [embeddedWalletAddress, setEmbeddedWalletAddress] = useState<
     string | undefined
   >();
   const [smartWalletAddress, setSmartWalletAddress] = useState<
     string | undefined
   >();
-  const [usdcAmount, setUsdcAmount] = useState("");
-  const [recipientAddress, setRecipientAddress] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const [isLoading, setIsLoading] = useState(false);
 
   const [isLoadingMintNft, setIsLoadingMintNft] = useState(false);
   const [errorMessageMintNft, setErrorMessageMintNft] = useState("");
@@ -59,75 +32,7 @@ export default function Home() {
 
   const { login } = useLogin();
   const { client } = useSmartWallets();
-
-  const { switchChain } = useSwitchChain();
   const chainId = useChainId();
-
-  const signMessage = async () => {
-    if (!client) {
-      console.error("No smart account client found");
-      return;
-    }
-    try {
-      const signature = await client.signMessage({
-        message,
-      });
-      console.log("Signature", signature);
-      setSignedMessage(signature);
-    } catch (error) {
-      console.error("Error signing message:", error);
-      setSignedMessage("Error signing message");
-    }
-  };
-
-  const toggleChain = async () => {
-    if (!client) {
-      console.error("No smart account client found");
-      return;
-    }
-    switchChain({
-      chainId: chainId === baseSepolia.id ? base.id : baseSepolia.id,
-    });
-  };
-
-  const execTransaction = async () => {
-    setIsLoading(true);
-    if (!client) {
-      console.error("No smart account client found");
-      return;
-    }
-
-    setErrorMessage("");
-
-    // Convert USDC amount to smallest unit (6 decimal places)
-    const amount = parseUnits(usdcAmount, 6);
-
-    if (smartUsdcBalance && amount > smartUsdcBalance) {
-      setErrorMessage("Insufficient USDC balance");
-      return;
-    }
-
-    try {
-      const tx = await client.sendTransaction({
-        to:
-          chainId === baseSepolia.id
-            ? BASE_SEPOLIA_USDC_ADDRESS
-            : BASE_USDC_ADDRESS,
-        value: BigInt(0),
-        data: encodeFunctionData({
-          abi: erc20Abi,
-          functionName: "transfer",
-          args: [recipientAddress as `0x${string}`, amount],
-        }),
-        account: client.account,
-      });
-      console.log("tx", tx);
-    } catch (error) {
-      console.error("Transaction failed:", error);
-      setErrorMessage("Transaction failed. Please try again.");
-    }
-    setIsLoading(false);
-  };
 
   const mintNftTransaction = async () => {
     setIsLoadingMintNft(true);
@@ -172,23 +77,9 @@ export default function Home() {
     []
   );
 
-  const copySignedMessage = async () => {
-    await navigator.clipboard.writeText(signedMessage);
-    setCopiedSignedMessage(true);
-    setTimeout(() => setCopiedSignedMessage(false), 2000);
-  };
-
-  const toggleSignedMessageExpansion = () => {
-    setIsSignedMessageExpanded(!isSignedMessageExpanded);
-  };
-
   const handleLogout = () => {
     // Reset all input fields
     setMessage("");
-    setUsdcAmount("");
-    setRecipientAddress("");
-    setSignedMessage("");
-    setErrorMessage("");
 
     setRecipientNftAddress("");
     setErrorMessageMintNft("");
@@ -205,34 +96,6 @@ export default function Home() {
     }
   }, [user, client]);
 
-  const { data: embeddedEthBalance } = useBalance({
-    address: embeddedWalletAddress as `0x${string}`,
-  });
-
-  const { data: smartEthBalance } = useBalance({
-    address: smartWalletAddress as `0x${string}`,
-  });
-
-  const { data: embeddedUsdcBalance } = useReadContract({
-    abi: erc20Abi,
-    address:
-      chainId === baseSepolia.id
-        ? BASE_SEPOLIA_USDC_ADDRESS
-        : BASE_USDC_ADDRESS,
-    functionName: "balanceOf",
-    args: [embeddedWalletAddress as `0x${string}`],
-  });
-
-  const { data: smartUsdcBalance } = useReadContract({
-    abi: erc20Abi,
-    address:
-      chainId === baseSepolia.id
-        ? BASE_SEPOLIA_USDC_ADDRESS
-        : BASE_USDC_ADDRESS,
-    functionName: "balanceOf",
-    args: [smartWalletAddress as `0x${string}`],
-  });
-
   return (
     <div className="min-h-screen min-w-screen">
       <div className="grid grid-cols-1 lg:grid-cols-4 h-screen text-black">
@@ -242,8 +105,7 @@ export default function Home() {
               <div className="flex flex-col gap-2"></div>
               <div className="text-3xl lg:text-6xl font-black">Proofifi</div>
               <div className="text-md lg:text-lg">
-                This app demonstrates how to use Privy Smart Wallets to sign and
-                execute transactions.
+                Create NFT labels for your second hand items.
               </div>
 
               {ready && !authenticated && (
@@ -335,7 +197,7 @@ export default function Home() {
                 </div>
                 <Divider />
                 <div className="flex flex-col gap-2">
-                  <div className="text-sm font-semibold">Mint NFT</div>
+                  <div className="text-sm font-semibold">Create label</div>
                   <Input
                     size="sm"
                     value={recipientNftAddress}
@@ -353,7 +215,7 @@ export default function Home() {
                   className="w-full"
                   isDisabled={!recipientNftAddress}
                 >
-                  Mint NFT
+                  Create label
                 </Button>
                 {errorMessageMintNft && (
                   <div className="text-red-500 text-xs text-center mt-1">
